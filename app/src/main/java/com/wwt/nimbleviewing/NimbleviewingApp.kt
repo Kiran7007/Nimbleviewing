2f1819ef065673535fe03fd669262f2a83305ad9
Kiran <karan.punwatkar@gmail.com>
2021-07-23 01:06:38 +0530

    Removed extra line

--- app/src/main/java/com/wwt/nimbleviewing/NimbleviewingApp.kt
+++ app/src/main/java/com/wwt/nimbleviewing/NimbleviewingApp.kt
@@ -6,7 +6,6 @@ import org.koin.core.context.startKoin
 class NimbleviewingApp : Application() {
     override fun onCreate() {
         super.onCreate()
-
         startKoin {
             modules(remoteModules, repositoryModule, viewmodelModule) // TODO: do we need DI yet?
         }
